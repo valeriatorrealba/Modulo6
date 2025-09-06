a564737b2ed7e6ff9cf358fff87d80d922e2b02f
Valeria <valeria.torrealba@gmail.com>
2025-09-06 16:43:50 -0400

    M6AE2 - ABP2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import UserList from './components/userList.vue';
+import UserList from './components/UserList.vue';
 import Form from './components/Form.vue';
+import Personajes from './components/Personajes.vue';
 </script>
 
 <template>
     <UserList />
+    <Personajes />
     <Form />
 
 </template>
---
+++ src/components/Personajes.vue
@@ -0,0 +1,30 @@
+<script>
+import { personajesData } from '../data/personajes.js'
+
+export default {
+    data() {
+        return {
+            personajes: personajesData
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container mt-5">
+        <h2 class="text-center mb-4">Lista de Pokemones</h2>
+
+        <div class="row">
+            <div v-for="personaje in personajes" :key="personaje.id" class="col-md-4 mb-3">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ personaje.nombre }}</h5>
+                        <p class="card-text">{{ personaje.descripcion }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
