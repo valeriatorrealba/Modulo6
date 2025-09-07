935a507625e5f299171b988c62f43f30709101cf
Valeria <valeria.torrealba@gmail.com>
2025-09-07 17:21:57 -0300

    M6AE3 - ABP3

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import UserList from './components/UserList.vue';
 import Form from './components/Form.vue';
 import Personajes from './components/Personajes.vue';
+import ForDos from './components/ForDos.vue';
 </script>
 
 <template>
+    <ForDos />
     <UserList />
     <Personajes />
     <Form />
