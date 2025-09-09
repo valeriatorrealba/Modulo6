3064074bae9ec32cb593e14ae4cb1f800f5e733f
Valeria <valeria.torrealba@gmail.com>
2025-09-08 21:50:11 -0300

    M6AE4 - ABP4

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import UserList from './components/UserList.vue';
 import Form from './components/Form.vue';
 import Personajes from './components/Personajes.vue';
 import ForDos from './components/ForDos.vue';
+import Pokemon from './components/Pokemon.vue';
 </script>
 
 <template>
+    <Pokemon />
     <ForDos />
     <UserList />
     <Personajes />
